5373e64d9f4b669ed3ae9bd2536d9a9cef6accd5
Gabriel Adetya Utomo <gabrielutomo33@gmail.com>
2026-06-16 19:13:01 +0700

    fix: adjust profile2.jpg position to center 30%

--- app/components/sections/ProfileCard.tsx
+++ app/components/sections/ProfileCard.tsx
@@ -82,7 +82,7 @@ const ProfileCard: React.FC<ProfileCardProps> = ({
                         fill
                         style={{
                             objectFit: 'cover',
-                            objectPosition: 'center 20%',
+                            objectPosition: 'center 30%',
                         }}
                         priority
                     />
